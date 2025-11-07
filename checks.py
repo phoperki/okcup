7becf4d7557fbb02979357849ee56182c8761b2f
Phoenix <phoenix@phoenixperkins.com>
2025-11-07 16:08:59 -0600

    fixed checks.py

--- checks.py
+++ checks.py
@@ -38,16 +38,51 @@ stig_fingings = whole_stig["stig"]["findings"]
 #         print(command)
 #     print()
 
-with open('run_checks.sh', 'w') as output_file:
-    output_file.write('#!/bin/bash\n\n')
+# with open('run_checks.sh', 'w') as output_file:
+#     output_file.write('#!/bin/bash\n\n')
 
-    for findingID in stig_fingings:
-        output_file.write(f"# {findingID} | {stig_fingings[findingID]["severity"]} | {stig_fingings[findingID]["title"]}\n")
-        output_file.write(f"echo '=== {findingID} | {stig_fingings[findingID]["severity"]} ==='\n")
-        checker = re.findall(r'^\s*\$ (.+)$', stig_fingings[findingID]["checktext"], re.MULTILINE)
+#     for findingID in stig_fingings:
+#         output_file.write(f"# {findingID} | {stig_fingings[findingID]["severity"]} | {stig_fingings[findingID]["title"]}\n")
+#         output_file.write(f"echo '=== {findingID} | {stig_fingings[findingID]["severity"]} ==='\n")
+#         checker = re.findall(r'^\s*\$ (.+)$', stig_fingings[findingID]["checktext"], re.MULTILINE)
 
-        for command in checker:
-            output_file.write(f"echo 'Running: {command}'\n")
-            output_file.write(command + '\n')
-        output_file.write("\n")
+#         for command in checker:
+#             output_file.write(f"echo 'Running: {command}'\n")
+#             output_file.write(command + '\n')
+#         output_file.write("\n")
     
+def high_checks():
+    with open('high_checks.sh', 'w') as output_file:
+        output_file.write('#!/bin/bash\n\n')
+        output_file.write('set +e\n\n')
+        for findingID in stig_fingings:
+            if stig_fingings[findingID]["severity"] == "high":
+                output_file.write(f"# {findingID} | {stig_fingings[findingID]["severity"]} | {stig_fingings[findingID]["title"]}\n")
+                output_file.write(f"echo '=== {findingID} | {stig_fingings[findingID]["severity"]} ==='\n")
+                checker = re.findall(r'^\s*\$ (.+)$', stig_fingings[findingID]["checktext"], re.MULTILINE)
+
+                for command in checker:
+                    output_file.write(f"# 'Running: {command}'\n")
+                    output_file.write(command + '\n')
+                output_file.write("\n")
+
+high_checks()
+
+
+def medium_checks():
+    with open('medium_checks.sh', 'w') as output_file:
+        output_file.write('#!/bin/bash\n\n')
+        output_file.write('set +e\n\n')
+        
+        for findingID in stig_fingings:
+            if stig_fingings[findingID]["severity"] == "medium":
+                output_file.write(f"# {findingID} | {stig_fingings[findingID]["severity"]} | {stig_fingings[findingID]["title"]}\n")
+                output_file.write(f"echo '=== {findingID} | {stig_fingings[findingID]["severity"]} ==='\n")
+                checker = re.findall(r'^\s*\$ (.+)$', stig_fingings[findingID]["checktext"], re.MULTILINE)
+
+                for command in checker:
+                    output_file.write(f"# 'Running: {command}'\n")
+                    output_file.write(command + '\n')
+                output_file.write("\n")
+
+medium_checks()
